b27b50cd27b4c02d46a2aee143dab51804184088
lion hua <632105841@qq.com>
2019-11-30 16:24:53 +0800

    Initial commit

---
+++ src/main/java/com/luxshare/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.luxshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka server
+ *
+ * @author lion hua
+ * @since 2019-11-29
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
